e5f7449d4e7c4304f5f6c1fdfa200d570900a3be
Andrew Garcia <58530453+ag-codes@users.noreply.github.com>
2019-12-14 17:25:29 -0500

    Updates

---
+++ precache-manifest.044b9473d9d1c75fde51449dfc8eab96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f4e7a5b4a82bec93d196e6e67dd84b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "12c19fa2099049cb59fa",
+    "url": "/monsters-rolodex/static/css/main.3521a55e.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "12c19fa2099049cb59fa",
+    "url": "/monsters-rolodex/static/js/main.e5d6f2b8.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
